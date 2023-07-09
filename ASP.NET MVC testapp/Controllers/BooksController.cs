6931dadaae38b5d7a1a4edd3cbf98d98db6ba476
Vladyslav <volkoff.vladd@gmail.com>
2023-07-09 21:24:16 +0200

    project clearing

--- ASP.NET MVC testapp/Controllers/BooksController.cs
+++ ASP.NET MVC testapp/Controllers/BooksController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ASP.NET_MVC_testapp.Models;
-using ASP.NET_MVC_testapp.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 
--- ASP.NET MVC testapp/Repository/BookInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using ASP.NET_MVC_testapp.Models;
-
-namespace ASP.NET_MVC_testapp
-{
-    public interface BookInterface
-    {
-        List<Book> SearchBooks(string searchTerm);
-    }
-}
