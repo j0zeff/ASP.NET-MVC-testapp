53e61535e9b9d43f9d448eb5f8090d9e5bcc3419
samoilov <borova21@ukr.net>
2023-07-25 15:02:06 +0200

    +MyBooksController

---
+++ ASP.NET MVC testapp/Controllers/MyBooksController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC_testapp.Controllers
+{
+    public class MyBooksController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
